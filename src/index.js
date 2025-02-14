136feda548d96276a01d761b89278f45b6141363
Daniel Moura <daniel.bnh13@gmail.com>
2025-02-14 19:35:19 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './home';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { useState, useEffect } from 'react';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
